da9d0cfc7c4565f1a337bb89793744ed9e6ca2b0
Manh8008 <125587030+Manh8008@users.noreply.github.com>
2024-08-07 12:54:29 +0700

    Hí anh em

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
-        return {
-            env: {
-                BACKEND_URL: 'http://localhost:5000', // URL backend khi chạy local
-            },
-        };
-    }
-
-    return {
-        env: {
-            BACKEND_URL: process.env.BACKEND_URL || 'nodejs-app-1-sn84.onrender.com',
-        },
-    };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        BACKEND_URL: process.env.BACKEND_URL || 'https://nodejs-app-prlp.onrender.com',
+    },
 };
+
+export default nextConfig;
