3a80496530d0a541bcc2674b2ad07adf7f08676e
Manh8008 <125587030+Manh8008@users.noreply.github.com>
2024-09-09 17:44:44 +0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:5000',
+        BACKEND_URL: process.env.BACKEND_URL || 'https://nodejs-app-inky.vercel.app',
     },
     images: {
         domains: ['localhost'],
