82f05ad8bcf417dbf9d913294f6f4d6b57401b64
Manh8008 <125587030+Manh8008@users.noreply.github.com>
2024-09-17 00:42:32 +0700

    fix bug Can't resolve './Home.module.scss'

--- app/(store)/(pages)/checkout/loading.jsx
+++ app/(store)/(pages)/checkout/loading.jsx
@@ -1,18 +1,7 @@
-import classNames from 'classnames/bind'
 import { LoadingSkeleton } from '@/components/ui'
-import gridStyles from '@/assets/styles/grid.module.scss'
-import styles from './Home.module.scss'
-const cx = classNames.bind(styles)
-const gx = classNames.bind(gridStyles)
 
 function Loading() {
-    return (
-        <div className={gx('grid', 'wide')}>
-            <div className={cx('wrapper')}>
-                <LoadingSkeleton />
-            </div>
-        </div>
-    )
+    return <LoadingSkeleton />
 }
 
 export default Loading
