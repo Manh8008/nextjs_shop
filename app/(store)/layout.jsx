d7da6cf995f8803aa82a8dad0be429815dacbbbb
Manh8008 <125587030+Manh8008@users.noreply.github.com>
2024-10-29 09:21:29 +0700

    test

--- app/(store)/layout.jsx
+++ app/(store)/layout.jsx
@@ -4,6 +4,7 @@ import '@/assets/styles/variable.scss'
 import '@/assets/styles/grid.scss'
 import './globalStyle.scss'
 import 'rc-slider/assets/index.css'
+import Head from 'next/head'
 
 export const metadata = {
     charSet: 'utf8',
@@ -14,7 +15,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
     return (
         <html lang="en">
-            <head>
+            <Head>
                 <meta charSet={metadata.charSet} />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>{metadata.title}</title>
@@ -24,13 +25,26 @@ const RootLayout = ({ children }) => {
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
                 />
+
                 <link
                     rel="icon"
                     href="https://pubcdn.ivymoda.com/ivy2/images/logo-icon.ico"
                     type="image/png"
                     sizes="16x16"
-                ></link>
-            </head>
+                />
+                {/* Google tag (gtag.js) */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-XLKNZBX3E4"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-XLKNZBX3E4');
+                        `
+                    }}
+                />
+            </Head>
 
             <body suppressHydrationWarning={true}>
                 <Providers>
